e2a101aee4a794ac7c731a67be88e13cb89a16d7
Francis Iwu <apkfem@gmail.com>
2025-07-08 18:50:22 +0200

    implementing auth using Neon Auth and Stack Auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,29 @@
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
-import { HomeIcon, Sprout } from 'lucide-react'
+import { HomeIcon, LogInIcon, LogOutIcon, Sprout } from 'lucide-react'
 import { ModeToggle } from './ModeToggle'
+import { stackServerApp } from '@/stack'
+import { getUserDetails } from '@/actions/user.action'
+import { UserButton } from '@stackframe/stack'
 
-function Navbar() {
-  return (
+ async function Navbar() {
+   const user = await stackServerApp.getUser();
+   const app = stackServerApp.urls;
+   const userProfile = await getUserDetails(user?.id);
+   return (
     <nav className='sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50'>
      <div className='max-w-7xl mx-auto px-4'>
         <div className='flex items-center h-16 justify-between'>
+            {/* LOGO */}
         <div className="flex items-center">
             <Link href="/" className='text-xl font-bold text-primary font-mono tracking-wider'>
             CRUD APP
             </Link>
         </div>
+
+         {/* NAV LINKS */}
+          
 
          <div className="space-x-4 items-center md:flex hidden">
             <Button variant="ghost" className='flex items-center gap-2 asChild'>
@@ -29,6 +39,25 @@ function Navbar() {
                 </Link>
             </Button>
             <ModeToggle />
+
+{/* SIGN IN BUTTON */}
+{user ? ( <> <Button variant="ghost" className='flex items-center gap-2 asChild'>
+                <Link href={app.signOut}>
+                <LogOutIcon className='w-4 h-4' />
+                 <span className='hidden lg:inline'>Sign Out</span>
+                </Link>
+            </Button>
+            <UserButton/>
+            </>) : (<><Button variant="ghost" className='flex items-center gap-2 asChild'>
+                <Link href={app.signIn}>
+                <LogInIcon className='w-4 h-4' />
+                 <span className='hidden lg:inline'>Sign In</span>
+                </Link>
+            </Button></>)}
+            
+           
+           
+
          </div>
          </div>
      </div>
